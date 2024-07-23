48ed1cb004ba63aa1e36eb4b6c529bcd2dad6c8f
Mark Connelly <mark.d.connelly@outlook.com>
2024-07-23 00:32:18 -0500

    Update dev-datapipeline-build-01.tf

---
+++ 3.WorkloadSpokes/dev-datapipeline-build-01.tf
@@ -0,0 +1,21 @@
+#region Building Blocks
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "Ent_DevOps_DataPipeline-01_RG" {
+  name     = "Ent_DevOps_DataPipeline-01_RG"
+  location = "Central US"
+          tags = {
+        environment = "production"
+        costcenter = "DevOps"
+        description = "Resource group for general data pipeline services in the DevOps environment. Critical business operations are dependent on these pipelines."
+    }
+}
+
+resource "azurerm_data_factory" "adf-devops-dp-01" {
+  name                = "adf-devops-dp-01"
+  location            = azurerm_virtual_wan_hub.vHub-CUS-01.location
+  resource_group_name = azurerm_resource_group.Ent_DevOps_DataPipeline-01_RG.name
+}
+
