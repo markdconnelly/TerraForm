06ba4d8aa8671ef993e5b702f153c305e5914389
Mark.Connelly <mark.d.connelly@outlook.com>
2024-07-23 10:24:32 -0500

    Update dev-datapipeline-build-01.tf

--- 3.WorkloadSpokes/dev-datapipeline-build-01.tf
+++ 3.WorkloadSpokes/dev-datapipeline-build-01.tf
@@ -67,6 +67,34 @@ resource "azurerm_key_vault_key" "key-adf-devops-dp-01-rotate1yr" {
   }
 }
 
+resource "azurerm_key_vault_key" "key-stgdevopsdp01-rotate1yr" {
+  name         = "key-stgdevopsdp01-rotate1yr"
+  key_vault_id = azurerm_key_vault.kv-it-infrastructure-encryption.id
+  key_type     = "RSA"
+  key_size     = 4096
+  # fix activation and expiration dates here
+  not_before_date = "2021-01-01T00:00:00+00:00"
+  expiration_date = "2022-01-01T00:00:00+00:00"
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  # fix rotation policy logic here
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P3M"
+    }
+
+    expire_after         = "P1Y"
+    notify_before_expiry = "P30D"
+  }
+}
+
 # probably too granular of a scope, but illustrates
 resource "azurerm_role_assignment" "roleassign-key-adf-devops-dp-01-rotate1yr" {
   scope                = azurerm_key_vault_key.key-adf-devops-dp-01-rotate1yr.id
@@ -150,10 +178,43 @@ resource "azurerm_data_factory" "adf-devops-dp-01" {
 #region Storage Account
 resource "azurerm_storage_account" "stgdevopsdp01" {
   name                     = "stgdevopsdp01"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  resource_group_name      = azurerm_resource_group.Ent_DevOps_DataPipeline-01_RG.name
+  location                 = azurerm_virtual_wan_hub.vHub-CUS-01.location
+  default_to_oauth_authentication = true
+  account_kind = "StorageV2"
+  account_tier             = "Premium"
+  account_replication_type = "RAGZRS"
+  access_tier = "Hot"
+  enable_https_traffic_only = true
+  min_tls_version = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled = false
+  infrastructure_encryption_enabled = true
+  allowed_copy_scope = "PrivateLink"
+  dns_endpoint_type = "AzureDnsZone"
+  routing {
+    choice = "MicrosoftRouting"
+    publish_internet_endpoints = false
+    publish_microsoft_endpoints = true
+  }
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["10.0.0.0/8"]
+    bypass = "AzureServices"
+    virtual_network_subnet_ids = [subnet-cus-datapipeline-01-default.id, subnet-eus-datapipeline-01-default.id]
+    private_link_access {
+      
+    }
+  }
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.mgid-ent-kv-infrastructure-encryption.id]
+  }
+  customer_managed_key {
+    key_vault_key_id = azurerm_key_vault_key.key-stgdevopsdp01-rotate1yr.id
+    user_assigned_identity_id = azurerm_user_assigned_identity.mgid-ent-kv-infrastructure-encryption.id
+  
+  }
 }
 #endregion
 
